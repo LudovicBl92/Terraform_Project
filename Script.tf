670ea397b52727356df73892751bd924d5615d50
LudovicBl92 <ludo.blondiau@gmail.com>
2021-05-26 19:12:28 +0200

    add the first part of the script

---
+++ Script.tf
@@ -0,0 +1,48 @@
+resource "aws_vpc" "main" {
+    cidr_block = "10.0.0.0/16"
+
+    tags = {
+        Name = "Back"
+    }
+}
+
+resource "aws_subnet" "Pub_Network" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.0.0/24"
+    availability_zone = "eu-west-3a"
+
+    tags = {
+        Name = "Public"
+    }
+}
+
+resource "aws_subnet" "Priv_Network" {
+    vpc_id = "${aws_vpc.main.id}"
+    cidr_block = "10.0.1.0/24"
+    availability_zone = "eu-west-3a"
+
+    tags = {
+      Name = "Prive"
+    }
+}
+
+resource "aws_internet_gateway" "IG_Pub" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags = {
+        Name = "Ig_Pub"
+    }
+}
+
+resource "aws_route_table" "Route_Pub_Network" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    route {
+    cidr_block = "0.0.0.0/0" 
+    gateway_id = "${aws_internet_gateway.IG_Pub.id}"
+    }
+
+    tags = {
+        Name = "Route_Pub_Network"
+    }
+}
