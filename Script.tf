53441d7c19111463c6afd79ba552dbbf51679ebc
LudovicBl92 <ludo.blondiau@gmail.com>
2021-06-03 08:32:56 +0200

    Change name and reorganise script

--- Script.tf
+++ Script.tf
@@ -2,10 +2,17 @@ resource "aws_vpc" "main" {
     cidr_block = "10.0.0.0/16"
 
     tags = {
-        Name = "Back"
+        Name = "Project_Web"
     }
 }
 
+resource "aws_internet_gateway" "IG_Pub" {
+    vpc_id = "${aws_vpc.main.id}"
+
+    tags = {
+        Name = "Ig_Pub"
+    }
+}
 resource "aws_subnet" "Pub_Network" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.0.0/24"
@@ -19,18 +26,10 @@ resource "aws_subnet" "Pub_Network" {
 resource "aws_subnet" "Priv_Network" {
     vpc_id = "${aws_vpc.main.id}"
     cidr_block = "10.0.1.0/24"
-    availability_zone = "eu-west-3a"
+    availability_zone = "eu-west-3b"
 
     tags = {
-      Name = "Prive"
-    }
-}
-
-resource "aws_internet_gateway" "IG_Pub" {
-    vpc_id = "${aws_vpc.main.id}"
-
-    tags = {
-        Name = "Ig_Pub"
+      Name = "Private"
     }
 }
 
